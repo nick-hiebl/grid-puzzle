3b9b329b3be1610c220b4da2e0f64870069d0526
nick-hiebl <nicholas.hiebl@gmail.com>
2021-07-31 00:59:36 +1000

    Updated day 19 puzzles

--- src/pages/days/day19.tsx
+++ src/pages/days/day19.tsx
@@ -18,70 +18,81 @@ const PUZZLE_0 = {
 };
 const PUZZLE_1 = {
   n: 4,
-  rowClues: [Ec.SQ_1,Ec.YES_TRIPLES,Ec.SQ_1,Ec.SQ_1],
-  colClues: [Ec.NO_TRIPLES,Ec.YES_TRIPLES,Ec.YES_TRIPLES,Ec.SQ_1],
+  rowClues: [Ec.SQ_2,Ec.DIV_A_NO,Ec.SQ_1,Ec.DIV_A_YES],
+  colClues: [Ec.DIV_A_NO,4,Ec.DIV_A_NO,Ec.DIV_A_NO],
   globalFeatures,
+  gridFeatures: [step(2, 1)],
 };
 const PUZZLE_2 = {
   n: 4,
-  rowClues: [Ec.SQ_1,Ec.SQ_1,Ec.YES_TRIPLES,Ec.SQ_1],
-  colClues: [Ec.SQ_1,Ec.SQ_1,Ec.SQ_1,Ec.NO_TRIPLES],
-  minTotal: 7,
+  rowClues: [Ec.NONO_1_1,Ec.REFLECTIVE,null,Ec.REFLECTIVE],
+  colClues: [Ec.DIV_A_YES,Ec.SQ_1,4,Ec.DIV_A_YES],
   globalFeatures,
+  gridFeatures: [step(3, 1)]
 };
 const PUZZLE_3 = {
-  w: 6,
-  h: 4,
-  rowClues: [1,Ec.NONO_2_1,Ec.SQ_2,Ec.SQ_1],
-  colClues: [3,Ec.REFLECTIVE,2,Ec.SQ_1,3,Ec.REFLECTIVE],
+  n: 5,
+  rowClues: [Ec.SQ_1,Ec.SQ_1,Ec.SQ_1,Ec.NO_TRIPLES,Ec.NONO_3_1],
+  colClues: [Ec.REFLECTIVE,Ec.SQ_1,Ec.SQ_1,2,2],
   globalFeatures,
+  gridFeatures: [step(1, 3)],
 };
 const PUZZLE_4 = {
+  n: 5,
+  rowClues: [Ec.DIV_A_YES,null,3,Ec.SQ_1,Ec.DIV_A_NO],
+  colClues: [Ec.DIV_A_NO,Ec.REFLECTIVE,Ec.YES_TRIPLES,Ec.REFLECTIVE,1],
+  globalFeatures,
+  gridFeatures: [step(2, 2)],
+};
+const PUZZLE_5 = {
   w: 7,
-  h: 4,
-  rowClues: [Ec.SQ_2,Ec.NO_TRIPLES,Ec.SQ_3,Ec.SQ_3],
-  colClues: [0,Ec.REFLECTIVE,null,2,Ec.SQ_1,Ec.YES_TRIPLES,3],
+  h: 5,
+  rowClues: [Ec.REFLECTIVE,Ec.NO_TRIPLES,Ec.REFLECTIVE,Ec.NONO_1_2_2,5],
+  colClues: [3,Ec.SQ_1,Ec.SQ_1,3,Ec.SQ_1,Ec.SQ_1,Ec.REFLECTIVE],
   globalFeatures,
+  gridFeatures: [step(6, 3)],
 };
-
-const FINAL_PUZZLE_1 = {
-  w: 8,
-  h: 6,
-  rowClues: [Ec.NO_TRIPLES,Ec.REFLECTIVE,Ec.REFLECTIVE,Ec.SQ_2,Ec.REFLECTIVE,8],
-  colClues: [2,Ec.REFLECTIVE,Ec.SQ_1,null,Ec.SQ_1,1,6,Ec.SQ_1],
+const PUZZLE_6 = {
+  w: 7,
+  h: 5,
+  rowClues: [1,Ec.SQ_2,Ec.NO_TRIPLES,Ec.NONO_4_1,Ec.SQ_1],
+  colClues: [Ec.REFLECTIVE,0,Ec.REFLECTIVE,Ec.SQ_1,3,null,1],
   globalFeatures,
+  gridFeatures: [step(0, 3)],
 };
-const FINAL_PUZZLE_2 = {
-  w: 8,
-  h: 6,
-  rowClues: [Ec.SQ_3,Ec.NONO_3_2,6,Ec.YES_TRIPLES,6,8],
-  colClues: [Ec.SQ_1,6,5,Ec.NO_TRIPLES,Ec.YES_TRIPLES,5,Ec.YES_TRIPLES,1],
+const PUZZLE_7 = {
+  w: 7,
+  h: 5,
+  rowClues: [Ec.NONO_1_1,3,Ec.NO_TRIPLES,Ec.NO_TRIPLES,Ec.NONO_1_4],
+  colClues: [null,Ec.SQ_1,Ec.REFLECTIVE,2,Ec.SQ_1,5,0],
   globalFeatures,
+  gridFeatures: [step(0, 2)],
+};
+const PUZZLE_8 = {
+  w: 7,
+  h: 5,
+  rowClues: [null,Ec.NONO_1_1,Ec.NONO_3_1,Ec.SQ_1,6],
+  colClues: [2,3,Ec.REFLECTIVE,null,Ec.SQ_1,3,5],
+  globalFeatures,
+  gridFeatures: [step(2, 1)],
 };
 
 const TodaysPage = (props: DayProps) => {
   return (
     <Container>
       {props.links}
-      <Typography variant="h3">Day 15: Piling up</Typography>
+      <Typography variant="h3">Day 19: A starting step to build on</Typography>
 
       <p>
-        An advanced variation on the mechanic from day 15. If you haven't yet,
-        make sure to try it out: <DayLink index={14} />
-      </p>
-
-      <p>
-        <strong>Note:</strong> For those who have been following along in real
-        time, the art for "nonogram clues" has been updated. It's not too
-        different, so you may be able to figure out how it works yourself, but
-        if you'd like to see the new explanation visit: <DayLink index={10} />
+        An additional feature to work with the mechanic from day 15. If you
+        haven't yet, make sure to try it out: <DayLink index={14} />
       </p>
 
       <Typography variant="h4">Intro Puzzle</Typography>
       <PuzzleWrapper details={PUZZLE_0} highlightErrors />
       <br />
 
-      {/* <Typography variant="h4">Puzzle #1</Typography>
+      <Typography variant="h4">Puzzle #1</Typography>
       <p>
         Time for some proper puzzles.
       </p>
@@ -100,15 +111,21 @@ const TodaysPage = (props: DayProps) => {
       <PuzzleWrapper details={PUZZLE_4} highlightErrors />
       <br />
 
-      <Typography variant="h4">Final Puzzle #1</Typography>
-      <p>Time for some big ones.</p>
-      <PuzzleWrapper details={FINAL_PUZZLE_1} highlightErrors />
+      <Typography variant="h4">Puzzle #5</Typography>
+      <PuzzleWrapper details={PUZZLE_5} highlightErrors />
       <br />
 
-      <Typography variant="h4">Final Puzzle #2</Typography>
-      <p>Final puzzle #2 is back!</p>
-      <PuzzleWrapper details={FINAL_PUZZLE_2} highlightErrors />
-      <br /> */}
+      <Typography variant="h4">Puzzle #6</Typography>
+      <PuzzleWrapper details={PUZZLE_6} highlightErrors />
+      <br />
+
+      <Typography variant="h4">Puzzle #7</Typography>
+      <PuzzleWrapper details={PUZZLE_7} highlightErrors />
+      <br />
+
+      <Typography variant="h4">Puzzle #8</Typography>
+      <PuzzleWrapper details={PUZZLE_8} highlightErrors />
+      <br />
 
       {props.links}
     </Container>
